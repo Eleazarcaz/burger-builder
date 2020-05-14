c832e2daa81412ecf5c73efc2965cde0bc01a4d6
Eleazar <eleazarcaz@outlook.com>
2020-05-13 19:12:57 -0500

    Salida de BurgerIngredient dinamico

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,10 +3,19 @@ import Auxiliar from '../../hoc/auxiliar';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    },
+  };
+
   render() {
     return (
       <Auxiliar>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </Auxiliar>
     );
